fixed prediction computation in tf model. surprisingly performs worse than my clf!

loss_functions.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+from tensorflow.keras.losses import BinaryCrossentropy  # type: ignore
 
 
 def safe_Y_hat(Y_hat, epsilon=1e-15):
@@ -43,10 +44,11 @@ class Loss(ABC):
 
 class BinaryCrossEntropyLoss(Loss):
     def _loss(self, Y_hat, Y) -> float:
-        # Y_hat are the predictions
-        Y_hat = safe_Y_hat(Y_hat)
-        loss = -1 * (Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
-        return np.mean(loss)
+        return BinaryCrossentropy(from_logits=False)(Y, Y_hat).numpy()
+        # # Y_hat are the predictions
+        # Y_hat = safe_Y_hat(Y_hat)
+        # loss = -1 * (Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
+        # return np.mean(loss)
 
     def _dloss_dy_hat(self, Y_hat, Y) -> np.array:
         n = Y_hat.shape[1]

tensor_flow.py
@@ -7,8 +7,7 @@ from tensorflow.keras.losses import BinaryCrossentropy  # type: ignore
 from tensorflow.keras.optimizers.legacy import Adam  # type: ignore
 
 
-def predict_with_threshold(model, input_data, threshold=0.5):
-    probabilities = tf.nn.sigmoid(model.predict(input_data)).numpy()
+def predict_with_threshold(probabilities, threshold=0.5):
     return (probabilities > threshold).astype(int)
 
 
@@ -37,13 +36,12 @@ def relu_neural_net(training_data, training_labels):
         ]
     )
     model.compile(
-        loss=BinaryCrossentropy(from_logits=True),
+        loss=BinaryCrossentropy(from_logits=False),
         optimizer=Adam(learning_rate=0.01),
     )
-
     model.fit(training_data, training_labels, epochs=100)
-    predictions = predict_with_threshold(model, training_data)
-    num_correct = (predictions == predictions).sum()
+    Y_hat = model.predict(training_data)
+    num_correct = (training_labels == predict_with_threshold(Y_hat)).sum()
     print(f"Accuracy: {num_correct / training_data.shape[0]}")
     print_weights(model)
 